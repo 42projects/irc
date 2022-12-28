c114dab427f1a62c82d022c6be277290d65c853a
Yub_2 <hyeok230@naver.com>
2022-12-28 20:55:29 +0900

    mandatory client

---
+++ include/Channel.hpp
@@ -0,0 +1,18 @@
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+
+#include <vector>
+#include <string>
+#include "Client.hpp"
+
+class Client;
+
+class Channel{
+
+private :
+	
+public :
+	
+};
+
+#endif
